3cb7a9a6349ceb3ef49e1d88c3f094c05acbcd52
Kevin Dutzy <kddutz15@gmail.com>
2023-01-10 12:39:05 -0500

    commit tree 497c7415940a1fd332106bf7dbdaa63d4e72f792

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,45 @@
+class SpicesController < ApplicationController
+
+    def index
+        spices = Spice.all
+        render json: spices, status: :ok
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = find_spice
+        if spice
+            spice.update(spice_params)
+            render json: spice, status: :accepted
+        else
+            render json: { error: "Spice not found" }, status: :not_found
+        end
+    end
+
+    def destroy
+        spice = find_spice
+        if spice
+            spice.destroy
+            head :no_content
+        else
+            render json: { error: "Spice not found" }, status: :not_found
+        end
+    end
+
+
+
+    private
+
+    def find_spice
+        Spice.find_by(id: params[:id])
+    end
+
+    def spice_params
+      params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+end
